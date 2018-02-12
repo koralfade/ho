8bb1e20e97112d377a396af8d1b679cfb0f24da7
koralfade <koralfade@yandex.ru>
2018-02-12 16:42:54 +0300

    migrate

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -18,7 +18,6 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('email', models.EmailField(max_length=254)),
                 ('name', models.CharField(max_length=128)),
-                ('phone_number', phonenumber_field.modelfields.PhoneNumberField(max_length=128)),
                 ('number_kids', models.IntegerField()),
                 ('number_adult', models.IntegerField()),
                 ('data', models.DateTimeField()),
